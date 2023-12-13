20c63352cb837941934de2a405d05c1a5b06c6d9
Craig Puxty <craig_puxty@talktalk.net>
2023-12-13 22:43:24 +0000

    Initial setup.

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+// React router
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+// Pages
+import Homepage from './pages/Homepage';
+import Product from './pages/Product';
+import Pricing from './pages/Pricing';
+import PageNotFound from './pages/PageNotFound';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='product' element={<Product />} />
+        <Route path='pricing' element={<Pricing />} />
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
