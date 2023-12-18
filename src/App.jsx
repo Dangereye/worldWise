83a24efea87371dee8b7fdbafcc4f0362c3f72d1
Craig Puxty <craig_puxty@talktalk.net>
2023-12-18 20:48:47 +0000

    Installed json-server & added server script. Added CityList & CityItem components. Updated App with cities and loading state.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+// React
+import { useEffect, useState } from 'react';
+
 // React router
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
@@ -9,7 +12,30 @@ import Login from './pages/Login';
 import AppLayout from './pages/AppLayout';
 import PageNotFound from './pages/PageNotFound';
 
+// Components
+import CityList from './components/CityList';
+
+const BASE_URL = 'http://localhost:8000';
+
 export default function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function getCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (error) {
+        alert('There was an error loading data... ');
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    getCities();
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
@@ -18,8 +44,14 @@ export default function App() {
         <Route path='pricing' element={<Pricing />} />
         <Route path='login' element={<Login />} />
         <Route path='app' element={<AppLayout />}>
-          <Route index element={<p>list of cities</p>} />
-          <Route path='cities' element={<p>list of cities</p>} />
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path='cities'
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
           <Route path='countries' element={<p>Countries</p>} />
           <Route path='form' element={<p>Form</p>} />
         </Route>
