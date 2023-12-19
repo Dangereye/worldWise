36769d278e6a3f0f086d292ac503169da47e7561
Craig Puxty <craig_puxty@talktalk.net>
2023-12-19 19:54:40 +0000

    Added Button component. Updated Form with Buttons and programmatic navigation.

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ import PageNotFound from './pages/PageNotFound';
 import CityList from './components/CityList';
 import CountryList from './components/CountryList';
 import City from './components/City';
+import Form from './components/Form';
 
 const BASE_URL = 'http://localhost:8000';
 
@@ -60,7 +61,7 @@ export default function App() {
             path='countries'
             element={<CountryList cities={cities} isLoading={isLoading} />}
           />
-          <Route path='form' element={<p>Form</p>} />
+          <Route path='form' element={<Form />} />
         </Route>
         <Route path='*' element={<PageNotFound />} />
       </Routes>
